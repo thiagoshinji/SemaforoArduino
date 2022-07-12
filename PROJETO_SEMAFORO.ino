026a42970a5889f1940cc6d4fa40080d1a4dd498
Your Name <you@example.com>
2022-07-12 15:56:45 -0300

    first commit

---
+++ PROJETO_SEMAFORO.ino
@@ -0,0 +1,47 @@
+void setup() {
+  pinMode(12,OUTPUT);
+  pinMode(11,OUTPUT);
+  pinMode(10,OUTPUT);
+  pinMode(9,OUTPUT);
+  pinMode(8,OUTPUT);
+}
+
+void loop() {
+ 
+  digitalWrite(12,HIGH);  
+  delay(50);           
+  digitalWrite(12,LOW);
+   
+  digitalWrite(11,HIGH);
+  delay(50);
+  digitalWrite(11,LOW);
+   
+  digitalWrite(10,HIGH);
+  delay(50);
+  digitalWrite(10,LOW);
+
+  digitalWrite(9,HIGH);
+  delay(50);
+  digitalWrite(9,LOW);
+
+  digitalWrite(8,HIGH);
+  delay(50);
+  digitalWrite(8,LOW);
+
+  digitalWrite(9,HIGH);
+  delay(50);
+  digitalWrite(9,LOW);
+
+    digitalWrite(10,HIGH);
+  delay(50);
+  digitalWrite(10,LOW);
+
+    digitalWrite(11,HIGH);
+  delay(50);
+  digitalWrite(11,LOW);
+
+    digitalWrite(12,HIGH);  
+  delay(50);           
+  digitalWrite(12,LOW);
+ 
+}
